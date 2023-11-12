39fb8d2e5ee05bf7c18a3a965358042228092d8e
Heo Yun Jeong <127824349+hunjyeong@users.noreply.github.com>
2023-11-12 18:15:02 +0900

    Delete MPT directory

--- MPT/README.md
+++
@@ -1,2 +0,0 @@
-# merkle-patricia-trie
-A Merkle Patricia Trie implementation for study.
--- MPT/nibble.hpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-using namespace std;
-
-class Nibble {
-    public:
-        static int MatchedLength(string n1, string n2);
-
-};
